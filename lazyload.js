4ad898cb45e33c05181ce9e325be4e68f0fad324
greenxevious <49845934+dominicanderson1185@users.noreply.github.com>
2020-02-20 16:32:30 -0700

    redone

--- lazyload.js
+++ lazyload.js
@@ -8,20 +8,35 @@ const loadImages = (image) => {
 imagesToLoad.forEach((img) => {
   loadImages(img);
 });
-if('IntersectionObserver' in window) {
-  const observer = new IntersectionObserver((items, observer) => {
-    items.forEach((item) => {
-      if(item.isIntersecting) {
-        loadImages(item.target);
-        observer.unobserve(item.target);
-      }
-    });
-  });
-  imagesToLoad.forEach((img) => {
-    observer.observe(img);
-  });
-} else {
-  imagesToLoad.forEach((img) => {
-    loadImages(img);
-  });
+
+const images = document.querySelectorAll("[data-src]");
+
+function preloadImage(img){
+const src = img.getAttribute("data-src");
+if(!src){
+    return;
 }
+img.src = src;
+
+}
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px -300px 0"
+};
+
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) =>
+{entries.forEach(entry =>
+    { if(!entry.isIntersecting){
+        return;
+    }else{
+    preloadImage(entry.target);
+    imgObserver.unobserve(entry.target);
+        }
+})
+},imgOptions);
+
+
+images.forEach(image => {imgObserver.observe(image);}
+);
